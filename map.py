43f796f1337f4cfc0f43b5b1bdb887a2eb1d892a
Tom Mariani <tom.mariani@etu.minesparis.psl.eu>
2023-01-27 15:00:24 +0100

    ajouts niveau et quelques assets (de niveau 2 à 4)

--- map.py
+++ map.py
@@ -10,25 +10,60 @@ class Map():
         [7, 0, 0, 0, 0, 0, 0, 0, 3], 
         [7, 0, 0, 0, 0, 0, 0, 0, 3],
         [7, 0, 0, 0, 0, 0, 0, 0, 3],
-        [7, 20, 0, 0, 8, 5, 2, 0, 3],
+        [7, 0, 0, 0, 8, 5, 2, 0, 3],
         [6, 5, 5, 5, 4, 9, 6, 5, 4],
     ])
 
+    niv2 = np.array([
+        [7, 1, 1, 1, 1, 1, 1, 1, 3, 9, 9, 7, 1, 1, 1, 1, 3],
+        [7, 0, 0, 10, 0, 0, 0, 0, 3, 9, 9, 7, 0, 0, 0, 13, 3], 
+        [7, 0, 0, 0, 11, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 3],
+        [7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 5, 4],
+        [7, 0, 10, 0, 12, 0, 8, 5, 5, 5, 5, 5, 5, 5, 4, 9, 9],
+        [7, 0, 0, 0, 0, 0, 3, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9],
+        [7, 0, 0, 0, 0, 0, 3, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9],
+        [7, 0, 13, 0, 0, 0, 3, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9],
+        [6, 5, 5, 5, 5, 5, 4, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9],
+    ])
 
+    niv3 = np.array([
+        [7, 1, 1, 1, 1, 1, 3],
+        [7, 0, 0, 0, 0, 0, 3], 
+        [7, 0, 0, 0, 0, 0, 3],
+        [6, 5, 5, 5, 5, 5, 4],
+    ])
+
+    niv4 = np.array([
+        [7, 1, 1, 1, 1, 1, 3, 9, 9, 9, 9, 9, 9],
+        [7, 0, 0, 0, 0, 0, 3, 9, 9, 9, 9, 9, 9], 
+        [7, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 3, 9],
+        [7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 9],
+        [7, 0, 0, 0, 0, 0, 8, 5, 5, 2, 0, 3, 9],
+        [7, 0, 0, 0, 0, 0, 3, 9, 9, 7, 0, 3, 9],
+        [7, 0, 0, 0, 0, 0, 3, 9, 9, 7, 0, 3, 9],
+        [6, 5, 5, 5, 5, 5, 4, 9, 9, 7, 0, 3, 9],
+        [9, 7, 1, 1, 1, 1, 1, 1, 1, 1, 0, 3, 9],
+        [9, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 9],
+        [9, 7, 0, 0, 0, 0, 8, 5, 5, 5, 5, 4, 9],
+        [9, 7, 0, 0, 0, 0, 3, 9, 9, 9, 9, 9, 9],
+        [9, 6, 5, 5, 5, 5, 4, 9, 9, 9, 9, 9, 9]])
+
+    niv2echelle = map(np.where(niv2 == 13), [niv1, niv3])
+
+    niv3echelle = map(np.where(niv2 == 13), [niv2])
+    
+
+    
 
     def get_size(self, map):
         w, h = map.shape
         return (h * self.pixelcase, w * self.pixelcase)
 
     def showMap(self, map, fenetre):
-        fenetre.blit(Assets.assetDungeon, (0,0), Assets.fondsize)
-        fenetre.blit(Assets.assetDungeon, (4*32,0), Assets.fondsize)
-        fenetre.blit(Assets.assetDungeon, (0,3*32), Assets.fondsize)
-        fenetre.blit(Assets.assetDungeon, (4*32,3*32), Assets.fondsize)
-        fenetre.blit(Assets.assetDungeon, (8*32,0), Assets.fondsize)
-        fenetre.blit(Assets.assetDungeon, (8*32,3*32), Assets.fondsize)
-        fenetre.blit(Assets.assetDungeon, (8*32,6*32), Assets.fondsize)
-        fenetre.blit(Assets.assetDungeon, (4*32,6*32), Assets.fondsize)
+        for i in range(5):
+            for j in range(5):
+                fenetre.blit(Assets.assetDungeon, (i*4*32,j*3*32), Assets.fondsize)
+            
         for j, c in enumerate(map):
             for i, case in enumerate(c):
 
@@ -53,6 +88,16 @@ class Map():
                     fenetre.blit(Assets.assetDungeon, (x,y), Assets.murtopleftsize)
                 if case == 9:
                     fenetre.blit(Assets.assetDungeon, (x,y), Assets.murpleinsize)
+
+
+                if case == 10:
+                    fenetre.blit(Assets.assetDungeon, (x,y), Assets.moneysize)
+                if case == 11:
+                    fenetre.blit(Assets.assetDungeon, (x,y), Assets.potionsize)
+                if case == 12:
+                    fenetre.blit(Assets.assetDungeon, (x,y), Assets.trapsize)
+                if case == 13:
+                    fenetre.blit(Assets.assetDungeon, (x,y), Assets.echellesize)
 
                 if case == 20:
                     fenetre.blit(Assets.assetPerso, (x,y), Assets.persosize)
