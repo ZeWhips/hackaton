fbb1a139634bde0b8395bc78467f9bcec2ecda66
Tristan Montalbetti <tristan.montalbetti@etu.minesparis.psl.eu>
2023-01-27 15:45:59 +0100

    zihvi

--- player.py
+++ player.py
@@ -53,3 +53,35 @@ class Player:
                 map[x][y] = 20
         return [x, y]
     
+
+
+    def collect_coin(self):
+        '''To call after moving the player and before displaying'''
+        x, y = self.getPos()
+        if self._map[x][y] == 10: #Coin
+            self._capital += 1
+            message = 'You got a coin and win 1 point of capital'
+        return message
+
+
+    def fall_in_trap(self):
+        '''To call after moving the player and before displaying'''
+        x, y = self.getPos()
+        if self._map[x][y] == 12: #Trap
+            self._capital += -2
+            message = 'You fell in a trap and lose 2 points of capital'
+        return message
+
+
+    def collect_potion(self):
+        '''To call after moving the player and before displaying'''
+        x, y = self.getPos()
+        if self._map[x][y] == 11: #Potion
+            self._capital += 3
+            message = 'You got a magic potion and win 3 points of capital'
+        return message
+
+
+    def death(self, running):
+        if self._capital < 0:
+            running = False
