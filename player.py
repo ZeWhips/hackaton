1dd37110d5c032cb9dc4e22bacbe364338b6b00f
Tristan Montalbetti <tristan.montalbetti@etu.minesparis.psl.eu>
2023-01-27 12:12:12 +0100

    ajout de rencontre mur

--- player.py
+++ player.py
@@ -4,8 +4,8 @@ class player:
       
         self._pos = position 
 
-    def state(self, position, direction):
-      
+    def state(self, position, direction, map):
+        self._map = map
         self._pos = position 
         self._direction = direction
 
@@ -14,17 +14,33 @@ class player:
 
     def getDirection(self):
         return self._direction
+
+    def getMap(self):
+        return self._map
    
     def move_head(self):
         head = self.getPos()
+        map = self.getMap()
         x, y = head
         direction = self.getDirection()
-        if direction == 'up' :
-            x, y = x, y-1
+        if direction == 'up':
+            if 0 < map[x][y-1] < 10:
+                return "rencontre un mur "
+            else: 
+                x, y = x, y-1
         elif direction == 'down' :
-            x, y = x, y+1
+            if 0 < map[x][y+1] < 10:
+                return "rencontre un mur "
+            else:
+                x, y = x, y+1
         elif direction == 'left' :
-            x, y = x-1, y
+            if 0 < map[x-1][y] < 10:
+                return "rencontre un mur "
+            else:
+                x, y = x-1, y
         elif direction == 'right' :
-            x, y = x+1, y
+            if 0 < map[x+1][y] < 10:
+                return "rencontre un mur "
+            else:
+                x, y = x+1, y
         return (x, y)
